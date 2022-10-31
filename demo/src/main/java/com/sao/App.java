72777fc2ec6e910d3394a8e63972aa54459fcc95
WangPengJu <cyrwpj@163.com>
2022-10-31 12:03:12 +0800

    1.测试代码移动到App.java里面

--- demo/src/main/java/com/sao/App.java
+++ demo/src/main/java/com/sao/App.java
@@ -1,5 +1,8 @@
 package com.sao;
 
+import com.sao.Items.ItemTypes.Potion;
+import com.sao.Player.Player;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Player player1 = new Player(0, "晓铜");
+        System.out.println(player1.getStats());
+        Potion po = new Potion(0, "红药水", " +10 HP", 20, 5, 1);
+        player1.getBag().addChild(po);
+        System.out.println();
+        //查看药水背包:
+        System.out.println("药水背包: ");
+        player1.getBag().getCategoryBag(5).print();;
+        System.out.println(player1.getBag());
+        System.out.println("\n 介绍 : " + po.getName() + po + ", " + " 重量 " + po.getWeight() + " 价值 " + po.getPrice());
     }
 }
